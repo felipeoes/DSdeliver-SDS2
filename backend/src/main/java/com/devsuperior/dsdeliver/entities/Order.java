972d610a5c2e8de62ee250cf0c9f3c7022032b21
Felipe <felipeoes@usp.br>
2021-01-08 16:49:59 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Order.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
+public class Order implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
