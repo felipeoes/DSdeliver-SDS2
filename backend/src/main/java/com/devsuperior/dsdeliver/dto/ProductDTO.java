453fdf44be43f5dd688a11a19d37fbfbd3a9d56c
Felipe <felipeoes@usp.br>
2021-01-08 22:51:38 -0300

    Ready to prod

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/ProductDTO.java
@@ -71,6 +71,10 @@ public class ProductDTO implements Serializable{
 	public void setImageUri(String imageUri) {
 		this.imageUri = imageUri;
 	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	
 
 }
