50c8b0959201f229076072e4e206692332bcddb9
Felipe Oliveira <felipeoes@usp.br>
2021-01-19 13:35:59 -0300

    Added Home page

---
+++ front-app/src/Home/index.tsx
@@ -0,0 +1,79 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import Header from '../Header';
+
+function Home() {
+  const navigation = useNavigation();
+
+  const handleOnPress = () => {
+    navigation.navigate('Orders');
+  }
+
+  return (
+    <>
+      <Header />
+      <View style={styles.container}>
+        <Image source={require('../assets/deliveryman.png')} />
+        <Text style={styles.title}>
+          Peça sua refeição e {'\n'} receba no prazo indicado
+      </Text>
+        <Text style={styles.subTitle}>São milhares de restaurantes {'\n'}
+      empenhados para entregar o melhor pra você
+      </Text>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleOnPress}>
+          <Text style={styles.buttonText}>
+            FAÇA SEU PEDIDO
+            </Text>
+        </RectButton>
+      </View> 
+
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: '30%',
+    alignItems: 'center'
+  },
+  title: {
+    color: '#263238',
+    fontSize: 26,
+    lineHeight: 35,
+    fontWeight: 'bold',
+    marginTop: 31,
+    textAlign: 'center'
+  },
+  subTitle: {
+    color: '#9E9E9E',
+    fontSize: 16,
+    marginTop: 15,
+    lineHeight: 22,
+    textAlign: 'center'
+  },
+  footer: {
+    marginTop: '5%',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#DA5C5C',
+    flexDirection: 'row',
+    borderRadius: 10
+  },
+  buttonText: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 50,
+    paddingRight: 50,
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#FFF',
+    letterSpacing: -0.24
+  }
+});
+
+export default Home;
---
+++ front-app/src/api.ts
@@ -0,0 +1,15 @@
+// import axios from "axios";
+
+// const API_URL = 'https://felipe-sds2-project.herokuapp.com';
+
+// export function fetchProducts() {
+//     return axios(`${API_URL}/products`)
+// }
+
+// // export function fetchLocalMapBox(local: string) {
+// //     return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+// // }
+
+// // export function saveOrder(payload: OrderPayload) {
+// //     return axios.post(`${API_URL}/orders`, payload);
+// // }
